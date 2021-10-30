ae87f0180a2807034df5ef0f5921f695bf956e95
cywhale <cyrain@gmail.com>
2021-10-30 13:50:41 +0800

    add fastify mercurius-cache and redis-server for GraphQL query

--- app/src/srvapp.js
+++ app/src/srvapp.js
@@ -1,10 +1,12 @@
 //import Mongodb from 'fastify-mongodb'
 import AutoLoad from 'fastify-autoload'
 import { join } from 'desm'
-import mercurius from 'mercurius';
-import db from './config/db';
-import schema from './graphql/schema';
-import resolvers from './graphql/resolvers';
+import redis from 'fastify-redis'
+import mercurius from 'mercurius'
+import cache from 'mercurius-cache'
+import db from './config/db'
+import schema from './graphql/schema'
+import resolvers from './graphql/resolvers'
 
 export default async function (fastify, opts) {
   fastify.decorate('conf', {
@@ -15,7 +17,7 @@ export default async function (fastify, opts) {
     sessiondir: process.env.NODE_ENV === 'production'? '/session' : '/sessioninfo'
   })
 
-  fastify.register(db, { url: fastify.config.MONGO_CONNECT }); //use mongoose
+  fastify.register(db, { url: fastify.config.MONGO_CONNECT }) //use mongoose
 //fastify.register(logger, {})
 /*
   fastify.register(Mongodb, {
@@ -29,7 +31,43 @@ export default async function (fastify, opts) {
         resolvers: resolvers,
         graphiql: true, //'playground', //has been removed from mercuius issue #453
         jit: 1
-  });
+  })
+
+  fastify.register(redis)
+
+  const ttl = 60 * 60
+
+  fastify.register(cache, {
+    policy: {
+      Query: {
+        taxontree: true,
+        infq: true
+      }
+    },
+    storage: {
+      get: async function (key) {
+        try {
+          return JSON.parse(await fastify.redis.get(key))
+        } catch (err) {
+          fastify.log.error({ msg: 'error on get from redis', err, key })
+        }
+        return null
+      },
+      set: async function (key, value) {
+        try {
+          await fastify.redis.set(key, JSON.stringify(value), 'EX', ttl)
+        } catch (err) {
+          fastify.log.error({ msg: 'error on set into redis', err, key })
+        }
+      }
+    },
+    onHit: function (type, fieldName) {
+      fastify.log.info({ msg: 'hit from cache', type, fieldName })
+    },
+    onMiss: function (type, fieldName) {
+      fastify.log.info({ msg: 'miss from cache', type, fieldName })
+    }
+  })
 
   fastify.register(AutoLoad, {
       dir: join(import.meta.url, 'routes'),
